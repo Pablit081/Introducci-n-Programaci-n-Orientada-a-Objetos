f0e27195ca4b10f74b45efd448e0bfac53d38806
Pablit081 <pabariled@gmail.com>
2025-11-19 21:26:09 -0300

    Se crea el atributo estado, se crea el getEstado y el setEstado

--- TPO_Final/clase_Animal.php
+++ TPO_Final/clase_Animal.php
@@ -4,39 +4,34 @@ use Medoo\Medoo;
 
 abstract class Animal //Definimos la clase padre abstracta Animal
 {
-    protected $id;
+    protected $idAnimal;
     protected $nombre;
     protected $edad;
     protected $tipoAnimal;
+    protected $estado;
 
-public function __construct($nombre, $edad, $tipoAnimal) //Definimos el constructor de la clase Animal
+    public function __construct($nombre, $edad, $tipoAnimal) //Definimos el constructor de la clase Animal
 {
     $this->nombre = $nombre;
     $this->edad = $edad;
     $this->tipoAnimal = $tipoAnimal;
 }
 
-public function getNombre() //Definimos el metodo getter
-{
-    return $this->nombre;
-}
+public function getId() {return $this->idAnimal;} //Definimos el método getId
+public function getNombre() {return $this->nombre;} //Definimos el metodo getNombre
 
-public function getEdad() //Definimos el método getEdad
-{
-    return $this->edad;
-}
 
-public function getTipoAnimal() //Definimos el método getTipoAnimal
-{
-    return $this->tipoAnimal;
-}
+public function getEdad() {return $this->edad;} //Definimos el método getEdad
+
+
+public function getTipoAnimal() {return $this->tipoAnimal;} //Definimos el método getTipoAnimal
+
 
 abstract public function getCaracteristicasEspecificas(): string; //Definimos el método getCaracteristicasEspecificas
 
-public function esAdoptable(): bool //Definimos el método esAdotable
-{
-    return true;
+public function esAdoptable(): bool {return true;} //Definimos el método esAdotable (siempre devuelve true)
 
-}
+public function getEstado() {return $this->estado;} //Definimos el método getEstado
+public function setEstado($estado) {$this->estado = $estado;} //Definimos el método setEstado
 
 }
